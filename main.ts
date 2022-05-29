083e556aec93b54d55847201a431e5c12139e9e5
buzz1980 <105562851+buzz1980@users.noreply.github.com>
2022-05-29 07:41:31 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,83 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # # #
+        `)
+})
